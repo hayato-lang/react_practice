a8ce69990d06b76055546b9fc3a28bb3908f24f7
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-26 09:19:12 +0900

    準備

---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <p>Page1です</p>
+    </div>
+  );
+}
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <p>Page2です</p>
+    </div>
+  );
+}

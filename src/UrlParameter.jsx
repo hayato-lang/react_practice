2b48184f648918448ec6d5b316e2dd9053b451d2
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-27 13:18:01 +0900

    クエリパラメーターを扱う

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
+import { useLocation } from "react-router";
 import { useParams } from "react-router";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
   return (
     <div>
       <h1>UrlParameterです</h1>
       <p>パラメーターは{ id }です</p>
+      <p>クエリパラメーターは{ query.get("name") }です</p>
     </div>
   );
 }

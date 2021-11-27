6e2e4a4787fb355f3f31428b5e4592aa72656708
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-28 07:12:08 +0900

    Linkを使わないページ遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,16 @@
+import { useHistory } from "react-router";
 import { useLocation } from "react-router";
 
 export const Page1DetailA = () => {
   const {state} = useLocation();
+  const history = useHistory();
   console.log(state);
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1>Page1DetailAです</h1>
+      <br />
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 }
